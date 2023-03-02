47bd1d8514fffbbb3a88af7bc41284525d5ccb9a
Diego Fernandes <diego.schell.f@gmail.com>
2023-03-02 11:15:18 -0300

    Initial commit

---
+++ prisma/migrations/20230302140626_create_users/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
---
+++ src/server.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from '@prisma/client'
+import fastify from 'fastify'
+import { z } from 'zod'
+
+const app = fastify()
+
+const prisma = new PrismaClient()
+
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+
+  return { users }
+})
+
+app.post('/users', async (request, reply) => {
+  const createUserSchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+  })
+
+  const { name, email } = createUserSchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+    }
+  })
+
+  return reply.status(201).send()
+})
+
+app.listen({
+  host: '0.0.0.0',
+  port: process.env.PORT ? Number(process.env.PORT) : 3333,
+}).then(() => {
+  console.log('HTTP Server Running')
+})
